8c373f39c644f194e3705572755ff6552794ac44
sxh705 <2362989228@qq.com>
2022-12-10 10:02:04 +0800

    vue Study

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    {path: '/', name: 'home', component: HomeView},
+    {path: '/about', name: 'about', component: () => import('../views/AboutView')},
+    {path: '/sxh', name: 'sxh', component: () => import('../views/Sxh')},
+    {path: '/:pathMatch(.*)*', name: '404', component: () => import('../views/404')}
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ src/views/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>404 Not Found</h1>
+    <h2>你来到了没有知识的荒原!</h2>
+    <a href="/">回到首页</a>
+  </div>
+</template>
